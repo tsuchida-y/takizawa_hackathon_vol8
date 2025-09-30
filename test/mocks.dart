f06d438a124424bdcc9fb1cae55bcf06c67913f0
tsuchida <yu41926t.tutida@icloud.com>
2025-09-30 13:07:43 +0900

    chore:mockを自動で生成した

---
+++ test/mocks.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:takizawa_hackathon_vol8/providers/user_profile_provider.dart';
+
+
+@GenerateMocks([UserProfileRepository])
+void main() {}
